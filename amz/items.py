5d501bfd925c8862c0bf0d3be98f60d5d3a1350c
Elsayed_Aboelmaaty <eng.sayed.gamal@gmail.com>
2023-03-28 12:25:51 +0200

    first spider

--- amz/items.py
+++ amz/items.py
@@ -8,5 +8,8 @@ import scrapy
 
 class AmzItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    product_name = scrapy.Field()
+    product_author = scrapy.Field()
+    product_price = scrapy.Field()
+    product_imagelink = scrapy.Field()
+
